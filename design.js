5722c16e412cb7a3111fdd76d922718ae15993ad
Meaghan Grant <meaghangrant@Meaghans-MacBook-Pro.local>
2020-09-13 10:30:09 -0400

    finish

--- design.js
+++ design.js
@@ -1,1 +1,48 @@
+d3.select("#theAirplaneShow")
+    .on("click",function()
+{
+  d3.select("#planes")
+  .classed("hidden",false);
+})
 
+d3.select("#plane1")
+    .on("click",function()
+{
+   d3.select("#banner") 
+    .text("This is the first plane.");
+})
+
+d3.select("#plane2")
+    .on("click",function()
+{
+   d3.select("#banner")
+    .text("This is the second plane.");
+})
+
+d3.select("#plane3")
+    .on("click",function()
+{
+    d3.select("#banner")
+        .text("This is the third plane.");
+})
+
+var setPlane4 = function(img)
+{
+     d3.select("#plane4")
+        .attr("src",img);
+}
+d3.select("#plane4")
+    .on("mouseenter",function()
+{
+    setPlane4("Pics/NASM-NASM2020-00141-000001.jpeg");
+})
+    .on("mouseleave",function()
+    {
+       setPlane4("Pics/NASM-A19600300000-NASM2018-10346-000001.jpeg");
+    })
+d3.select("#plane4")
+    .on("clcik",function()
+    {
+        d3.select("#banner")
+            .text("This is plane four and five.");
+    })
